3977071b8aaf14043f1dbc6725f3616239db2e65
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2022-01-10 20:35:58 -0500

    [main_app] created a file called urls.py in main_app and added a path, created httpresonse in views.py, added urlpatterns to ROT13/urls.py

--- ROT13/main_app/views.py
+++ ROT13/main_app/views.py
@@ -1,3 +1,6 @@
+# main_app/views.py
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+  return HttpResponse('<h1>Hello World!</h1>')
